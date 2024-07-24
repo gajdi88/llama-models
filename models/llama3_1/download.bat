a77a588e4e4182f0605c27a5cf4e0f2a32fac8e6
gajdi88 <70482516+gajdi88@users.noreply.github.com>
2024-07-24 22:58:51 +0100

    translated download.sh to download.bat

--- models/llama3_1/download.bat
+++ models/llama3_1/download.bat
@@ -1,73 +1,157 @@
-:: Copyright (c) Meta Platforms, Inc. and affiliates.
-:: This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+@echo off
+REM Copyright (c) Meta Platforms, Inc. and affiliates.
+REM All rights reserved.
+REM
+REM This source code is licensed under the terms described in the LICENSE file in
+REM top-level folder for each specific model found within the models/ directory at
+REM the top-level of this source tree.
+REM
+REM Copyright (c) Meta Platforms, Inc. and affiliates.
+REM This software may be used and distributed according to the terms of the Llama 3.1 Community License Agreement.
 
-:: Prerequisites:
-:: 0. Windows machine
-:: 1. wget installed
-:: 2. You've broken up your presigned url into its URL variables (separated by "?" and "&")
+setlocal enabledelayedexpansion
 
-:: RUN AT YOUR OWN RISK: I removed the md5sum dependency because I couldn't get the windows version to work right with this script.
+REM Temporarily add wget to the PATH
+set PATH=C:\Program Files (x86)\GnuWin32\bin;%PATH%
 
-:: Example:
-:: download.bat YOUR_POLICY YOUR_SIGNATURE YOUR_KPID 7B .
+echo %PATH%
 
-:: Model size options = 7B,13B,70B,7B-chat,13B-chat,70B-chat
-:: Unlike the bash script, this only takes one model at a time
-
-:: First argument = Policy field from presigned url
-:: Second argument = Signature field from presigned url
-:: Third argument = Key-Pair-Id field from presigned url
-:: Fourth argument = model size (only one at a time)
-:: Fifth argument = target folder
-
-:: WARNING: This will NOT work in PowerShell
-:: WARNING: You must run cmd.exe as Administrator. I have a cmd shortcut on my desktop that I right click then click "Run as Administrator"
-
-@ECHO Off
-
-SET POLICY=%1
-SET SIGNATURE=%2
-SET KEY_PAIR_ID=%3
-SET MODEL_SIZE=%4
-SET TARGET_FOLDER=%5
-
-IF %MODEL_SIZE%=="" SET MODEL_SIZE=7B
-IF %TARGET_FOLDER%=="" SET TARGET_FOLDER=.
-
-ECHO "Downloading LICENSE and Acceptable Usage Policy"
-wget "https://download.llamameta.net/LICENSE?Policy=%POLICY%&Signature=%SIGNATURE%&Key-Pair-Id=%KEY_PAIR_ID%" -O "%TARGET_FOLDER%/LICENSE"
-wget "https://download.llamameta.net/USE_POLICY.md?Policy=%POLICY%&Signature=%SIGNATURE%&Key-Pair-Id=%KEY_PAIR_ID%" -O "%TARGET_FOLDER%/USE_POLICY.md"
-
-ECHO "Downloading tokenizer"
-wget "https://download.llamameta.net/tokenizer.model?Policy=%POLICY%&Signature=%SIGNATURE%&Key-Pair-Id=%KEY_PAIR_ID%" -O "%TARGET_FOLDER%/tokenizer.model"
-wget "https://download.llamameta.net/tokenizer_checklist.chk?Policy=%POLICY%&Signature=%SIGNATURE%&Key-Pair-Id=%KEY_PAIR_ID%" -O "%TARGET_FOLDER%/tokenizer_checklist.chk"
-
-IF %MODEL_SIZE%==7B SET /A SHARD=0
-IF %MODEL_SIZE%==7B SET MODEL_PATH=llama-2-7b
-
-if %MODEL_SIZE%==7B-chat SET /A SHARD=0
-if %MODEL_SIZE%==7B-chat SET MODEL_PATH=llama-2-7b-chat
-
-if %MODEL_SIZE%==13B SET /A SHARD=1
-if %MODEL_SIZE%==13B SET MODEL_PATH=llama-2-13b
-
-if %MODEL_SIZE%==13B-chat SET /A SHARD=1
-if %MODEL_SIZE%==13B-chat SET MODEL_PATH=llama-2-13b-chat
-
-if %MODEL_SIZE%==70B SET /A SHARD=7
-if %MODEL_SIZE%==70B SET MODEL_PATH=llama-2-70b
-
-if %MODEL_SIZE%==70B-chat SET /A SHARD=7
-if %MODEL_SIZE%==70B-chat SET MODEL_PATH=llama-2-70b-chat
-
-ECHO "Downloading model to %MODEL_PATH% with shard %SHARD% because MODEL_SIZE is %MODEL_SIZE%"
-MKDIR "%TARGET_FOLDER%/%MODEL_PATH%"
-
-FOR %%s IN (0, 1, %SHARD%) DO (
-    wget "https://download.llamameta.net/%MODEL_PATH%/consolidated.%s%.pth?Policy=%POLICY%&Signature=%SIGNATURE%&Key-Pair-Id=%KEY_PAIR_ID%"  -O "%TARGET_FOLDER%/%MODEL_PATH%/consolidated.%s%.pth"
+set /p PRESIGNED_URL="Enter the URL from email: "
+set ALL_MODELS_LIST=meta-llama-3.1-405b,meta-llama-3.1-70b,meta-llama-3.1-8b,meta-llama-guard-3-8b,prompt-guard
+echo.
+echo **** Model list ****
+for %%a in (%ALL_MODELS_LIST:,= %) do (
+    echo -  %%a
 )
 
-wget "https://download.llamameta.net/%MODEL_PATH%/params.json?Policy=%POLICY%&Signature=%SIGNATURE%&Key-Pair-Id=%KEY_PAIR_ID%"  -O "%TARGET_FOLDER%/%MODEL_PATH%/params.json"
-wget "https://download.llamameta.net/%MODEL_PATH%/checklist.chk?Policy=%POLICY%&Signature=%SIGNATURE%&Key-Pair-Id=%KEY_PAIR_ID%"  -O "%TARGET_FOLDER%/%MODEL_PATH%/checklist.chk"
+set /p SELECTED_MODEL="Choose the model to download: "
+echo.
+echo Selected model: %SELECTED_MODEL%
+echo.
 
-ECHO DONE
+set SELECTED_MODELS=
+
+if "%SELECTED_MODEL%"=="meta-llama-3.1-405b" (
+    set MODEL_LIST=meta-llama-3.1-405b-instruct-mp16,meta-llama-3.1-405b-instruct-mp8,meta-llama-3.1-405b-instruct-fp8,meta-llama-3.1-405b-mp16,meta-llama-3.1-405b-mp8,meta-llama-3.1-405b-fp8
+) else if "%SELECTED_MODEL%"=="meta-llama-3.1-70b" (
+    set MODEL_LIST=meta-llama-3.1-70b-instruct,meta-llama-3.1-70b
+) else if "%SELECTED_MODEL%"=="meta-llama-3.1-8b" (
+    set MODEL_LIST=meta-llama-3.1-8b-instruct,meta-llama-3.1-8b
+) else if "%SELECTED_MODEL%"=="meta-llama-guard-3-8b" (
+    set MODEL_LIST=meta-llama-guard-3-8b-int8-hf,meta-llama-guard-3-8b
+) else if "%SELECTED_MODEL%"=="prompt-guard" (
+    set SELECTED_MODELS=prompt-guard
+    set MODEL_LIST=
+)
+
+if "%SELECTED_MODELS%"=="" (
+    echo.
+    echo **** Available models to download: ***
+    for %%a in (%MODEL_LIST:,= %) do (
+        echo -  %%a
+    )
+    set /p SELECTED_MODELS="Enter the list of models to download without spaces or press Enter for all: "
+)
+
+set TARGET_FOLDER="."
+if not exist %TARGET_FOLDER% (
+    mkdir %TARGET_FOLDER%
+)
+
+if "%SELECTED_MODELS%"=="" (
+    set SELECTED_MODELS=%MODEL_LIST%
+)
+
+if "%SELECTED_MODEL%"=="meta-llama-3.1-405b" (
+    echo.
+    echo Model requires significant storage and computational resources, occupying approximately 750GB of disk storage space and necessitating two nodes on MP16 for inferencing.
+    set /p ACK="Enter Y to continue: "
+    if /i not "%ACK%"=="Y" (
+        echo Exiting...
+        exit /b 1
+    )
+)
+
+echo Downloading LICENSE and Acceptable Usage Policy
+wget --continue %PRESIGNED_URL%:*=LICENSE -O %TARGET_FOLDER%\LICENSE
+wget --continue %PRESIGNED_URL%:*=USE_POLICY.md -O %TARGET_FOLDER%\USE_POLICY.md
+
+for %%m in (%SELECTED_MODELS:,= %) do (
+    set ADDITIONAL_FILES=
+    set TOKENIZER_MODEL=1
+    if "%%m"=="meta-llama-3.1-405b-instruct-mp16" (
+        set PTH_FILE_COUNT=15
+        set MODEL_PATH=Meta-Llama-3.1-405B-Instruct-MP16
+    ) else if "%%m"=="meta-llama-3.1-405b-instruct-mp8" (
+        set PTH_FILE_COUNT=7
+        set MODEL_PATH=Meta-Llama-3.1-405B-Instruct-MP8
+    ) else if "%%m"=="meta-llama-3.1-405b-instruct-fp8" (
+        set PTH_FILE_COUNT=7
+        set MODEL_PATH=Meta-Llama-3.1-405B-Instruct
+        set ADDITIONAL_FILES=fp8_scales_0.pt,fp8_scales_1.pt,fp8_scales_2.pt,fp8_scales_3.pt,fp8_scales_4.pt,fp8_scales_5.pt,fp8_scales_6.pt,fp8_scales_7.pt
+    ) else if "%%m"=="meta-llama-3.1-405b-mp16" (
+        set PTH_FILE_COUNT=15
+        set MODEL_PATH=Meta-Llama-3.1-405B-MP16
+    ) else if "%%m"=="meta-llama-3.1-405b-mp8" (
+        set PTH_FILE_COUNT=7
+        set MODEL_PATH=Meta-Llama-3.1-405B-MP8
+    ) else if "%%m"=="meta-llama-3.1-405b-fp8" (
+        set PTH_FILE_COUNT=7
+        set MODEL_PATH=Meta-Llama-3.1-405B
+    ) else if "%%m"=="meta-llama-3.1-70b-instruct" (
+        set PTH_FILE_COUNT=7
+        set MODEL_PATH=Meta-Llama-3.1-70B-Instruct
+    ) else if "%%m"=="meta-llama-3.1-70b" (
+        set PTH_FILE_COUNT=7
+        set MODEL_PATH=Meta-Llama-3.1-70B
+    ) else if "%%m"=="meta-llama-3.1-8b-instruct" (
+        set PTH_FILE_COUNT=0
+        set MODEL_PATH=Meta-Llama-3.1-8B-Instruct
+    ) else if "%%m"=="meta-llama-3.1-8b" (
+        set PTH_FILE_COUNT=0
+        set MODEL_PATH=Meta-Llama-3.1-8B
+    ) else if "%%m"=="meta-llama-guard-3-8b-int8-hf" (
+        set PTH_FILE_COUNT=-1
+        set MODEL_PATH=Meta-Llama-Guard-3-8B-INT8-HF
+        set ADDITIONAL_FILES=generation_config.json,model-00001-of-00002.safetensors,model-00002-of-00002.safetensors,model.safetensors.index.json,special_tokens_map.json,tokenizer_config.json,tokenizer.json
+        set TOKENIZER_MODEL=0
+    ) else if "%%m"=="meta-llama-guard-3-8b" (
+        set PTH_FILE_COUNT=0
+        set MODEL_PATH=Meta-Llama-Guard-3-8B
+    ) else if "%%m"=="prompt-guard" (
+        set PTH_FILE_COUNT=-1
+        set MODEL_PATH=Prompt-Guard
+        set ADDITIONAL_FILES=model.safetensors,special_tokens_map.json,tokenizer_config.json,tokenizer.json
+        set TOKENIZER_MODEL=0
+    )
+
+    echo.
+    echo ***Downloading !MODEL_PATH!***
+    if not exist %TARGET_FOLDER%\!MODEL_PATH! (
+        mkdir %TARGET_FOLDER%\!MODEL_PATH!
+    )
+
+    if "!TOKENIZER_MODEL!"=="1" (
+        echo Downloading tokenizer
+        wget --continue %PRESIGNED_URL:*=!MODEL_PATH!/tokenizer.model% -O %TARGET_FOLDER%\!MODEL_PATH!\tokenizer.model
+    )
+
+    if !PTH_FILE_COUNT! geq 0 (
+        for /l %%s in (0, 1, !PTH_FILE_COUNT!) do (
+            set s=00%%s
+            set s=!s:~-2!
+            echo Downloading consolidated.!s!.pth
+            wget --continue %PRESIGNED_URL:*=!MODEL_PATH!/consolidated.!s!.pth% -O %TARGET_FOLDER%\!MODEL_PATH!\consolidated.!s!.pth
+        )
+    )
+
+    for %%f in (!ADDITIONAL_FILES:,= !) do (
+        echo Downloading %%f...
+        wget --continue %PRESIGNED_URL:*=!MODEL_PATH!/%%f% -O %TARGET_FOLDER%\!MODEL_PATH!\%%f
+    )
+
+    if not "%%m"=="prompt-guard" if not "%%m"=="meta-llama-guard-3-8b-int8-hf" (
+        echo Downloading params.json...
+        wget --continue %PRESIGNED_URL:*=!MODEL_PATH!/params.json% -O %TARGET_FOLDER%\!MODEL_PATH!\params.json
+    )
+)
