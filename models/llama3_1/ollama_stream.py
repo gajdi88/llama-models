1b6888054544c46d1bec687e94440970c721ce48
gajdi88 <70482516+gajdi88@users.noreply.github.com>
2024-07-27 20:55:43 +0100

    streaming chat service

--- models/llama3_1/ollama_stream.py
+++ models/llama3_1/ollama_stream.py
@@ -1,10 +1,54 @@
+# import ollama
+#
+# stream = ollama.chat(
+#     model='llama3.1:8b',
+#     messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
+#     stream=True,
+# )
+#
+# for chunk in stream:
+#   print(chunk['message']['content'], end='', flush=True)
+
+import asyncio
+import argparse
+
 import ollama
 
-stream = ollama.chat(
-    model='llama3.1:8b',
-    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
-    stream=True,
-)
 
-for chunk in stream:
-  print(chunk['message']['content'], end='', flush=True)
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--speak', default=False, action='store_true')
+    args = parser.parse_args()
+
+    speaker = None
+
+    client = ollama.AsyncClient()
+
+    messages = []
+
+    while True:
+        if content_in := input('>>> '):
+            messages.append({'role': 'user', 'content': content_in})
+
+            content_out = ''
+            message = {'role': 'assistant', 'content': ''}
+            async for response in await client.chat(model='llama3.1:8b', messages=messages, stream=True):
+                if response['done']:
+                    messages.append(message)
+
+                content = response['message']['content']
+                print(content, end='', flush=True)
+
+                content_out += content
+
+                message['content'] += content
+
+
+            print()
+
+
+try:
+    asyncio.run(main())
+except (KeyboardInterrupt, EOFError):
+    ...
